d0131c7b5e4b0b802f929663f07899145c716e51
Daxrajsinh <daxrajsinh16@gmail.com>
2024-03-31 16:42:47 +0530

    commit-9

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -6,7 +6,7 @@ import CategoriesAPI from './api/CategoriesAPI'
 import axios from 'axios'
 
 export const GlobalState = createContext()
-
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const DataProvider = ({children}) =>{
     const [token, setToken] = useState(false)
@@ -16,7 +16,7 @@ export const DataProvider = ({children}) =>{
         const firstLogin = localStorage.getItem('firstLogin')
         if(firstLogin){
             const refreshToken = async () =>{
-                const res = await axios.get('/user/refresh_token')
+                const res = await axios.get(`${backendUrl}/user/refresh_token`)
         
                 setToken(res.data.accesstoken)
     
